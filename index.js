29e9793cf91f8ac92aec4187dfa54dcf4c4f0f76
ripple <40640834+Comingo@users.noreply.github.com>
2019-03-17 22:28:47 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ client.on("ready", () => {
   client.user.setActivity(`${client.guilds.size} серверов || %help`, {type: "WATCHING"})
 });
 
-client.on("guildCreate", (guild) => {
+client.on("message", (message) => {
+  if(message.content === `${prefix}help`)
+  {
+    message.channel.send(`**Возможности**\n\n%mute @ping - мут пользователя.\n%kick @ping reason - кик\n%ban @ping reason - ban\n\n**ВСЕ БУДЕТ ДОПОЛНЯТЬСЯ, БОТ В БЕТА-ТЕСТЕ**`)
+
+  }
+});
+
+client.on("guildCreate", guild => {
   let hue = client.channels.get("555426672225157132")
   const guildadd = new Discord.RichEmbed()
   .setTitle("Новый гилд.")
@@ -18,7 +26,7 @@ client.on("guildCreate", (guild) => {
   hue.send(guildadd);
 });
 
-client.on("guildDelete", (guild) => {
+client.on("guildCreate", guild => {
   let hue = client.channels.get("555426672225157132")
   const guildadd = new Discord.RichEmbed()
   .setTitle("Пока, гилд.")
@@ -68,5 +76,81 @@ client.on("message", (message) => {
   };
 });
 
+client.on("message", (message) => {
+  if(message.content.startsWith(`${prefix}mute`))
+  {
+    let mute = message.guild.roles.find(role => role.name === "AlfredMute");
+    let m = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(message.member.hasPermissions("MANAGE_MESSAGES"))
+    {
+      if(!mute) return message.reply("У вас нет роли `AlfredMute`. Создайте и настройте её.")
+      if(m.roles.has(mute.id)) {
+            m.removeRole(mute.id);
+        message.channel.send(`${m} был размучен ${message.author}`)
+          } else {
+            m.addRole(mute.id);
+        message.channel.send(`${m} был замучен ${message.author}`)
+          }
+     } else {
+      message.reply("недостаточно прав.")
+     }
+  };
+});
+
+client.on("message", (message) => {
+  let messageArray = message.content.split(" ");
+  let args = messageArray.slice(1);
+
+  if(message.content.startsWith(`${prefix}ban`))
+  {
+    let bu = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+    if(!bu) return message.channel.send(`Пользователь не найден.`);
+
+    let br = args.join(" ").slice(22);
+
+    if(!br) return message.channel.send(`Причина не указана.`);
+
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("У Вас нет прав администратора");
+
+    if(bu.hasPermission("ADMINISTRATOR")) return message.channel.send("У данного участника есть права администратора.");
+
+    let banEmbed = new Discord.RichEmbed()
+    .setDescription("~Отлетел~")
+    .setColor("RANDOM")
+    .setTimestamp()
+    .addField("Забанен", `${bu} с ID ${bu.id}`)
+    .addField("Забанен юзером", `<@${message.author.id}> с ID ${message.author.id}`)
+    .addField("Забанен через канал", message.channel)
+    .addField("Причина", br)
+    .setFooter("Время");
+
+    message.guild.member(bu).ban(br);
+    message.channel.send(banEmbed);
+  };
+});
+
+client.on("message", (message) => {
+  let messageArray = message.content.split(" ");
+  let args = messageArray.slice(1);
+
+  if(message.content.startsWith(`${prefix}kick`))
+  {
+    let ku = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+    if(!ku) return message.channel.send(`Пользователь не найден.`);
+
+    let kr = args.join(" ").slice(22);
+
+    if(!kr) return message.channel.send(`Причина не указана.`);
+
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("У Вас нет прав администратора");
+
+    if(ku.hasPermission("ADMINISTRATOR")) return message.channel.send("У данного участника есть права администратора.");
+
+    message.guild.member(ku).kick(kr);
+    message.channel.send("Пользователь успешно кикнут");
+  };
+});
 
 client.login(cfg.token);
